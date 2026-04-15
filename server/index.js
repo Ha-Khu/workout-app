3c7fa29793bf699275d18c7bc06ea2a086cfcac0
David <141168015+Ha-Khu@users.noreply.github.com>
2026-04-15 20:44:42 +0200

    feat: pripojenie auth routera na index.js

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT
+const authRouter = require('./routes/auth.js')
+
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/auth', authRouter)
 
 
 app.listen(port, ()=>{
