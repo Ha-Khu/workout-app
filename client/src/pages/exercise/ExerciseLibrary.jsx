759eb6168079ead742c5f555360ae5c397a5ca8d
David <141168015+Ha-Khu@users.noreply.github.com>
2026-04-26 15:09:22 +0200

    feat: ExercisLibrary - logika a API volania

--- client/src/pages/exercise/ExerciseLibrary.jsx
+++ client/src/pages/exercise/ExerciseLibrary.jsx
@@ -1,5 +1,37 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import {useNavigate, Link} from 'react-router-dom'
+
 function ExerciseLibrary(){
-  return <div>ExerciseLibrary</div>
+  
+  const [exercise, setExercise] = useState([])
+  const [name, setName] = useState("")
+  const [bodyPart, setBodyPart] = useState("")
+  const navigate = useNavigate()
+  const token = localStorage.getItem('token')
+
+    useEffect(()=>{
+    if(!token){
+      navigate("/login")
+      return
+    }
+    axios.get("http://localhost:5000/api/exercise/library",{
+      headers: {Authorization: `Bearer ${token}`}
+    }).then(function(response){
+      setExercise(response.data)
+    })
+  }, [])
+
+  function handleExercise(){
+    axios.post("http://localhost:5000/api/exercise/library", {name, body_part: bodyPart},{
+      headers: {Authorization: `Bearer ${token}`}
+    })
+    .then(function(response){
+      setExercise([...exercise, {name, body_part: bodyPart}])
+      setName("")
+      setBodyPart("")
+    })
+  }
 }
 
 export default ExerciseLibrary
