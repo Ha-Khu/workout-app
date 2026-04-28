aa3d0f84fd546b2ae19f95e2774e68e1828c60d2
David <141168015+Ha-Khu@users.noreply.github.com>
2026-04-28 19:13:16 +0200

    feat: NewWorkout- základ, useState, načítanie exercise library

--- client/src/pages/workout/NewWorkout.jsx
+++ client/src/pages/workout/NewWorkout.jsx
@@ -1,5 +1,31 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import {useNavigate, Link} from 'react-router-dom'
+
 function NewWorkout(){
-  return <div>NewWorkout</div>
+  const [date, setDate] = useState("")
+  const [exercise, setExercise] = useState([])
+  const [selectedExercises, setSelectedExercises] = useState([])
+  const [weight, setWeight] = useState("")
+  const [reps, setReps] = useState("")
+  const navigate = useNavigate()
+  const token = localStorage.getItem('token')
+
+  useEffect(()=>{
+    if(!token){
+      navigate("/login")
+      return
+    }
+    axios.get("http://localhost:5000/api/exercise/library", {
+      headers: {Authorization: `Bearer ${token}`}
+    }).then(function(response){
+      setExercise(response.data)
+    })
+  }, [])
+
+  function handleSaveWorkout(){
+    
+  }
 }
 
 export default NewWorkout
