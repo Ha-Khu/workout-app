b62aba604fc47d3644c1a9a685a3c164c1dd683b
David <141168015+Ha-Khu@users.noreply.github.com>
2026-05-13 19:26:30 +0200

    fix: pridanie redirect z / na /login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
@@ -10,6 +10,7 @@ import PR from './pages/PR'
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />}/>
       <Route path="/register" element={<Register />}/>
       <Route path="/login" element={<Login />}/>
       <Route path="/dashboard" element={<Dashboard />}/>
